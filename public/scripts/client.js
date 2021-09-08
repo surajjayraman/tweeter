56a381e6f47d144ca7806166c14e1445cec468a9
surajjayraman <suraj.jayraman@gmail.com>
2021-09-08 18:17:01 -0400

    Create function createTweetElement: use hardcoded tweet data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,3 +3,44 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+
+// Test / driver code (temporary). Eventually will get this from the server.
+$(document).ready(function() {
+    
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  };
+
+  // new tweet HTML with data filled
+  createTweetElement = function(tweetObj) {
+    //const time = createTimeAgo(tweetObj.created_at);
+    const $tweet = $(`        
+            <article class="tweet">
+            <header>
+            <div>
+            <h3><img src="${tweetObj.user.avatars}"> &ensp; ${tweetObj.user.name}</h3>
+            <h3 class="userID">${tweetObj.user.handle}</h3>
+            </div>
+            </header>
+            <p>${tweetObj.content.text}</p>
+            <footer>
+            <div>${tweetObj.created_at}</div>
+            <div class="icons"><i class="fas fa-flag"></i>  <i class="fas fa-retweet"></i> <i class="fas fa-heart"></i></div>
+            </footer>
+            </article>`);
+    return $tweet;
+  };
+      
+  const $tweet = createTweetElement(tweetData);
+  console.log($tweet); // to see what it looks like
+  $('#tweets-container').append($tweet);
+
+});
