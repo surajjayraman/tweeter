b53e791349438dab8871d8643cc3c472880b7161
surajjayraman <suraj.jayraman@gmail.com>
2021-09-08 21:38:21 -0400

    Submit new tweet: shows up on the page without page refresh.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,35 +4,8 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Test / driver code (temporary). Eventually will get this from the server.
 $(document).ready(function() {
     
-  // Fake data taken from initial-tweets.json
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-
   // Tweet management: show a list of tweets //
   const renderTweets = function(tweets) {
     // loops through tweets
